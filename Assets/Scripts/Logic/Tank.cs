cfd65d2f5d7c32f0f3e6e7d4f39fbf8080463ef6
Rainer <782174139@qq.com>
2019-06-11 22:23:14 +0800

    change model
    修改模型尺寸

--- Assets/Scripts/Logic/Tank.cs
+++ Assets/Scripts/Logic/Tank.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Tank : MonoBehaviour {
 
+    //移动速度
+    public float speed = 1;
     //炮塔
     public Transform turrent;
 
@@ -13,6 +15,8 @@ public class Tank : MonoBehaviour {
     //炮塔目标角度
     private float turrentRotTarget = 0;
 
+
+
 	// Use this for initialization
 	void Start () {
 		
@@ -20,6 +24,26 @@ public class Tank : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            transform.position += transform.forward * speed;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            transform.position += transform.forward * speed;        
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.eulerAngles = new Vector3(0, 270, 0);
+            transform.position += transform.forward * speed;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.eulerAngles = new Vector3(0, 90, 0);
+            transform.position += transform.forward * speed;
+        }
+
 	}
 }
