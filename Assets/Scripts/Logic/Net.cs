ae0a34083c49cb828905bfdf31274c67dd0a5a05
XingTaoHu <782174139@qq.com>
2019-10-25 16:39:29 +0800

    net

--- Assets/Scripts/Logic/Net.cs
+++ Assets/Scripts/Logic/Net.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Sockets;
 using UnityEngine.UI;
 using System;
+using System.Linq;
 
 public class Net : MonoBehaviour {
 
@@ -24,7 +25,11 @@ public class Net : MonoBehaviour {
     //接收缓冲区
     const int BUFFER_SIZE = 1024;
     byte[] readBuff = new byte[BUFFER_SIZE];
-  
+
+    //增加的粘包分包处理
+    int buffCount = 0;
+    byte[] lenBytes = new byte[sizeof(UInt32)];
+    Int32 msgLength = 0;
 
     void Start()
     { 
@@ -54,6 +59,7 @@ public class Net : MonoBehaviour {
         //Connect
         string host = hostInput.text;
         int port = int.Parse(portInput.text);
+        Debug.Log("连接服务器：host:" + host + ", port:" + port);
         socket.Connect(host, port);
         clientText.text = socket.LocalEndPoint.ToString();
         //Recv
@@ -67,10 +73,8 @@ public class Net : MonoBehaviour {
             //count是接收数据的大小
             int count = socket.EndReceive(ar);
             //数据处理
-            string str = System.Text.Encoding.UTF8.GetString(readBuff, 0, count);
-            if (recvStr.Length > 300)
-                recvStr = "";
-            recvStr += str + "\n";
+            buffCount += count;
+            ProcessData();
             //继续接收
             socket.BeginReceive(readBuff, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCb, null);
         }
@@ -81,14 +85,40 @@ public class Net : MonoBehaviour {
         }
     }
 
+    //数据处理
+    private void ProcessData()
+    {
+        //小于字节长度
+        if (buffCount > sizeof(Int32))
+            return;
+        //消息长度
+        Array.Copy(readBuff, lenBytes, sizeof(Int32));
+        msgLength = BitConverter.ToInt32(lenBytes, 0);
+        if (buffCount < msgLength + sizeof(Int32))
+            return;
+        //处理消息
+        string str = System.Text.Encoding.UTF8.GetString(readBuff, sizeof(Int32), msgLength);
+        recvStr = str;
+        //清除已处理的消息
+        int count = buffCount - msgLength - sizeof(Int32);
+        Array.Copy(readBuff, msgLength, readBuff, 0, count);
+        buffCount = count;
+        if (buffCount > 0)
+            ProcessData();
+    }
+
     public void Send() {
         string str = textInput.text;
-        byte[] bytes = System.Text.Encoding.Default.GetBytes(str);
+        byte[] bytes = System.Text.Encoding.UTF8.GetBytes(str);
+        byte[] length = BitConverter.GetBytes(bytes.Length);
+        byte[] sendbuff = length.Concat(bytes).ToArray();
         try
         {
-            socket.Send(bytes);
+            socket.Send(sendbuff);
         }
-        catch { }
+        catch (Exception e){
+            Debug.LogError("发送数据错误：" + e.Message);
+        }
     }
 
 }
