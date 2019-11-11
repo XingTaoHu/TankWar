17a31d5cca78814e696531ae4edaa5ba7a7b47c0
Rainer <782174139@qq.com>
2019-11-11 23:29:28 +0800

    netmgr

--- Assets/Scripts/Global/Root.cs
+++ Assets/Scripts/Global/Root.cs
@@ -12,6 +12,6 @@ public class Root : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        NetMgr.Update();
 	}
 }
---
+++ Assets/Scripts/Net/NetMgr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetMgr 
+{
+    public static Connection servConn = new Connection();
+    public static void Update()
+    {
+        servConn.Update();
+    }
+
+    //心跳
+    public static ProtocolBase GetHeatBeatProtocol()
+    {
+        ProtocolBytes protocol = new ProtocolBytes();
+        protocol.AddString("HeatBeat");
+        return protocol;
+    }
+
+}
