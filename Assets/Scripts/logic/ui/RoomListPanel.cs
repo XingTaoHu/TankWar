d1f85ca9e748c04113af319f8617f187a4338257
XingTaoHu <782174139@qq.com>
2019-11-18 20:42:47 +0800

    roomlist

---
+++ Assets/Scripts/logic/ui/RoomListPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomListPanel : PanelBase
+{
+
+    #region 生命周期
+    public override void Init(params object[] args)
+    {
+        base.Init(args);
+    }
+
+    public override void OnShowing()
+    {
+        base.OnShowing();
+    }
+    #endregion
+
+
+
+}
